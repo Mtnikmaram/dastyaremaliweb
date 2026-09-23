4b85672215e318ef072513387c171c0d86781f09
Mtnikmaram <111112957+Mtnikmaram@users.noreply.github.com>
2026-09-23 14:11:17 +0330

    Fix Android JVM target compatibility

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -10,7 +10,14 @@ android {
         minSdk = 26
         targetSdk = 35
         versionCode = 1
-        versionName = "1.0.1"
+        versionName = "1.0.2"
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions {
+        jvmTarget = "17"
     }
 }
 dependencies {
